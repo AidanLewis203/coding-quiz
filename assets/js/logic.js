a6af1155b5887d1981d6e507c1aa396940665a06
Aidan Lewis <lewis.coding203@gmail.com>
2024-02-09 13:32:10 -0700

    fixed logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,95 +1,4 @@
-// add variables
-/*var timerEl = document.getElementById("timer")
-var quizStart = document.getElementById('quizStart')
-var start = document.getElementById('start')
-var listEl = document.getElementById('list')
-var li1 = document.getElementById("li1");
-var li2 = document.getElementById("li2");
-var li3 = document.getElementById("li3");
-var li4 = document.getElementById("li4");
-var titleEl = document.getElementById('opening')
-var endText = document.getElementById('endText')
-var index = 0
-var currentQuestionIndex = 0
-var timeLeft = 0
-var correct = document.getElementById('correct')
-var buttonEl = document.getElementsByClassName('button')
-var article = document.article;
-var question = document.querySelector(".question");
-
-function endGame(){
-    clearInterval(timeInterval);
-    listEl.setAttribute('style', 'visibility: hidden;')
-    question.textContent = 'All Done'
-    endText.textContent = 'Your final score is ' + timeLeft
-}
-
-
-
-
-// add a timer
-function countdown() {
-    var timeLeft = 75;
-    var timeInterval = setInterval(function () {
-    if (timeLeft >= 0) {
-        timerEl.textContent = timeLeft
-        timeLeft--;
-    } else {
-        clearInterval(timeInterval);
-        listEl.setAttribute('style', 'visibility: hidden;')
-        question.textContent = 'All Done'
-        endText.textContent = 'Your final score is ' + timeLeft
-       
-    }
-    }, 1000);
-    
-}
-// add a start prompt for the quiz
-quizStart.addEventListener('click', function (){
-    start.setAttribute('style', 'visibility: hidden;') 
-    listEl.setAttribute('style', 'visibility: visible;')
-    titleEl.setAttribute('style', 'visibility: hidden;')
-    countdown()
-    switchQuestion()
-    
-});
-
-function switchQuestion() {
-    question.textContent = questions[index].title
-    li1.textContent = questions[index].options[0]
-    li2.textContent = questions[index].options[1]
-    li3.textContent = questions[index].options[2]
-    li4.textContent = questions[index].options[3]
-    index++
-
-    if (index === questions.length) {
-        endGame()
-    }
-    
-}
-
-function checkQuestion(event){
-    var currentQuestion = questions[currentQuestionIndex];
-if (event.target.textContent !== questions[currentQuestionIndex].answers) {
-    timeLeft -= 15
-    correct.textContent = 'Wrong!'
-    switchQuestion()
-} else {
-    correct.textContent = 'Correct!'
-    switchQuestion()
-}
-}
-
-
-
-li3.addEventListener('click', function(event){
-    checkQuestion(event)
-});
-*/
-
-
-// Quiz questions and answers
-  
+// Set up variables
   const startButton = document.getElementById('start-button');
   const questionContainer = document.getElementById('question-container');
   const questionElement = document.getElementById('question');
@@ -103,6 +12,7 @@ li3.addEventListener('click', function(event){
   
   startButton.addEventListener('click', startQuiz);
   
+  // Function to start the quiz
   function startQuiz() {
     startButton.classList.add('hide');
     questionContainer.classList.remove('hide');
@@ -110,6 +20,7 @@ li3.addEventListener('click', function(event){
     startTimer();
   }
   
+  // Function to start the timer
   function startTimer() {
     const timer = setInterval(() => {
       timeLeft--;
@@ -122,11 +33,13 @@ li3.addEventListener('click', function(event){
     }, 1000);
   }
   
+  // Function to set the next question
   function setNextQuestion() {
     resetState();
     showQuestion(quizData[currentQuestionIndex]);
   }
   
+  // Function to show the question
   function showQuestion(question) {
     questionElement.innerText = question.question;
     question.answers.forEach(answer => {
@@ -144,6 +57,7 @@ li3.addEventListener('click', function(event){
     }
   }
   
+  // Function for when the user selects an answer
   function selectAnswer(event) {
     const selectedButton = event.target;
     const correct = selectedButton.innerText === quizData[currentQuestionIndex].correctAnswer;
